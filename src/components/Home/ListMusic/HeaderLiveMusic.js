5e319ef9adc70eb4e421b47cf0667ab24fbf1b60
Anhdv <dvanh271295@gmail.com>
2021-07-16 18:50:57 +0700

    df

--- src/components/Home/ListMusic/HeaderLiveMusic.js
+++ src/components/Home/ListMusic/HeaderLiveMusic.js
@@ -1,9 +1,11 @@
 import React, {useState} from 'react'
 import styled from "styled-components";
 import WaveCircle from '../../../assets/img/wave-circle.png';
-import WaveLine from '../../../assets/img/wave-circle.png';
+import WaveLine from '../../../assets/img/wave-line.png';
 import volumeFull from '../../../assets/img/volume_full.svg';
-import {Dropdown, Menu, Slider} from "antd";
+import minusIcon from '../../../assets/img/minus.png';
+import plusIcon from '../../../assets/img/plus.png';
+import {Dropdown, Slider} from "antd";
 
 export default function HeaderLiveMusic() {
     const [value, setValue] = useState(30)
@@ -28,8 +30,19 @@ export default function HeaderLiveMusic() {
                 </BgNumberVolume>
                 <BoxVolume><img src={volumeFull} alt=""/></BoxVolume>
             </ItemBoxVolume>
-
         </BoxWaveCircle>
+        <BoxLineWaveTime>
+            <TimeLabel>02:23</TimeLabel>
+            <ImageWave><img src={WaveLine} alt=""/></ImageWave>
+            <TimeLabel>05:00</TimeLabel>
+        </BoxLineWaveTime>
+        <BoxIconVolume>
+            <IconVolume><img src={minusIcon} alt={""}/></IconVolume>
+            <BoxNumberTotal>
+                <NumberTotal>59</NumberTotal>
+            </BoxNumberTotal>
+            <IconVolume><img src={plusIcon} alt={""}/></IconVolume>
+        </BoxIconVolume>
     </div>
 }
 
@@ -91,4 +104,65 @@ const BoxNumberVolume = styled.div`
 `
 
 const BoxVolume = styled.div`
+`
+const BoxLineWaveTime = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding-left: 35px;
+    padding-right: 35px;
+`
+const TimeLabel = styled.div`
+    color: #797A80;
+    font-size: 16px;
+    font-family: Montserrat-Regular;
+    font-weight: bold;
+`
+
+const ImageWave = styled.div`
+`
+
+const BoxIconVolume = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-top: 25px;
+`
+
+const IconVolume = styled.div`
+    background: #261E26;
+    box-shadow: -5px -5px 10px 0px rgb(70 104 103 / 61%);
+    width: 36px;
+    height: 36px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 50%;
+    cursor: pointer;
+`
+
+const BoxNumberTotal = styled.div`
+    background: linear-gradient(140.17deg, #251b25 16.43%, #423742 85.15%);
+    width: 49px;
+    height: 49px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 50%;
+    margin-left: 32px;
+    margin-right: 32px;
+    padding: 2px;
+`
+
+const NumberTotal = styled.div`
+    background-color: #3e343e;
+    border-radius: 50%;
+    width: 100%;
+    height: 100%;
+    color: #4B9AA5;
+    font-size: 18px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-weight: bold;
 `
